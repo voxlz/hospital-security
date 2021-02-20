ba393ae3f9017dbe966995dc7ece77422f5ddc87
emil Friberg <emil.c.friberg@gmail.com>
2021-02-20 10:12:34 +0100

    Updated Journal

--- Journal.java
+++ Journal.java
@@ -20,6 +20,25 @@ public class Journal{
 		if(user.getUserType() == UserType.GOVERNMENT){
 			return true;
 		}
+		if(checkDivision){
+			return checkDivision;
+		}
+		return false;
+	}
+	
+	public boolean checkDivision(User user){
+		if(user.getDivision == null){
+			return false;
+		} else if(user.getDivision.equals(division)){
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean canWrite(User user){
+		if(user.equals(doctor)){
+			return true;
+		}
 		return false;
 	}
 }
