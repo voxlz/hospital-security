55d19b2316409f4c8fd955ab44bdb749bd0e2998
emil Friberg <emil.c.friberg@gmail.com>
2021-02-20 11:25:08 +0100

    Might not work, not compiled

--- User.java
+++ User.java
@@ -1,10 +1,14 @@
+import java.util.ArrayList;
+
 public class User {
 
 	private final UserType userType;
 	private final String id;
 	private String division;
+	private list<> patients;
 
 	public User(String id, UserType userType) {
+		patients = new ArrayList<User>();
 		this.id = id;
 		this.userType = userType;
 	}
@@ -43,5 +47,11 @@ public class User {
 		User u = (User) o;
 
 		return id.equals(u.getId());
+	}
+	
+	public void addPatient(User user){
+		if(userType == UserType.DOCTOR){
+			patients.add(user);
+		}
 	}
 }
