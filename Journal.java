d9fb30a748663e21883ea08ee3cfa3363a153345
Tripp1n <torben.nordtorp@icloud.com>
2021-02-20 10:25:08 +0100

    fix

--- Journal.java
+++ Journal.java
@@ -1,42 +1,42 @@
 package project2;
 
-public class Journal{
+public class Journal {
 	private User doctor;
 	private User nurse;
 	private User patient;
 	private String division;
-	
-	public Journal(User doctor, User nurse, User Patient){
+
+	public Journal(User doctor, User nurse, User patient, String division) {
 		this.doctor = doctor;
 		this.nurse = nurse;
 		this.patient = patient;
 		this.division = division;
 	}
-	
-	public boolean canRead(User user){
-		if(user.equals(doctor) || user.equals(nurse) || user.equals(patient)){
+
+	public boolean canRead(User user) {
+		if (user.equals(doctor) || user.equals(nurse) || user.equals(patient)) {
 			return true;
 		}
-		if(user.getUserType() == UserType.GOVERNMENT){
+		if (user.getUserType() == UserType.GOVERNMENT) {
 			return true;
 		}
-		if(checkDivision){
-			return checkDivision;
+		if (checkDivision(user)) {
+			return checkDivision(user);
 		}
 		return false;
 	}
-	
-	public boolean checkDivision(User user){
-		if(user.getDivision == null){
+
+	public boolean checkDivision(User user) {
+		if (user.getDivision() == null) {
 			return false;
-		} else if(user.getDivision.equals(division)){
+		} else if (user.getDivision().equals(division)) {
 			return true;
 		}
 		return false;
 	}
-	
-	public boolean canWrite(User user){
-		if(user.equals(doctor)){
+
+	public boolean canWrite(User user) {
+		if (user.equals(doctor)) {
 			return true;
 		}
 		return false;
