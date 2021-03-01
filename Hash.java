ffae3851e97477b1ec5fe4b8eea56f60e702ca7f
MikaelPham <38829570+MikaelPham@users.noreply.github.com>
2021-03-01 14:03:26 +0100

    Update Hash.java
    Generating salt in a separate function generateSalt

--- Hash.java
+++ Hash.java
@@ -2,16 +2,24 @@ import java.security.*;
 import java.nio.charset.StandardCharsets;
 
 public class Hash {
-
-  public void hashPassword(String password) throws NoSuchAlgorithmException {
+  private byte[] generateSalt() {
+    //A 16 byte salt or 32 character in hexString is generated
     SecureRandom random = new SecureRandom();
     byte[] salt = new byte[16];
     random.nextBytes(salt);
+    return salt;
+  }
+
+  public void hashPassword(String password) throws NoSuchAlgorithmException {
+    byte[] salt = generateSalt();
     MessageDigest md = MessageDigest.getInstance("SHA-256");
     md.update(salt);
     byte[] hashedPassword = md.digest(password.getBytes(StandardCharsets.UTF_8));
     StringBuffer hexString = new StringBuffer();
-      for (int i = 0; i < hashedPassword.length; i++) {
+     for (int i = 0; i < salt.length; i++) {
+         hexString.append(Integer.toHexString(0xFF & salt[i]));
+     }
+     for (int i = 0; i < hashedPassword.length; i++) {
          hexString.append(Integer.toHexString(0xFF & hashedPassword[i]));
       }
     System.out.println(hexString.toString());
