251af98567174f014b29eb878746474f70e68252
MikaelPham <38829570+MikaelPham@users.noreply.github.com>
2021-03-02 20:25:48 +0100

    Update Hash.java
    Verifying function should now be able to verify correct password.

--- Hash.java
+++ Hash.java
@@ -1,27 +1,45 @@
 import java.security.*;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 public class Hash {
-  private byte[] generateSalt() {
-    //A 16 byte salt or 32 character in hexString is generated
+  private static byte[] generateSalt() {
+    //A 16 byte salt or 24 character long String is generated
     SecureRandom random = new SecureRandom();
     byte[] salt = new byte[16];
     random.nextBytes(salt);
     return salt;
   }
 
-  public void hashPassword(String password) throws NoSuchAlgorithmException {
+  public static String hashPassword(String password) throws NoSuchAlgorithmException {
     byte[] salt = generateSalt();
     MessageDigest md = MessageDigest.getInstance("SHA-256");
+    md.reset();
     md.update(salt);
     byte[] hashedPassword = md.digest(password.getBytes(StandardCharsets.UTF_8));
     StringBuffer hexString = new StringBuffer();
-     for (int i = 0; i < salt.length; i++) {
-         hexString.append(Integer.toHexString(0xFF & salt[i]));
-     }
-     for (int i = 0; i < hashedPassword.length; i++) {
-         hexString.append(Integer.toHexString(0xFF & hashedPassword[i]));
-      }
+    hexString.append(Base64.getEncoder().encodeToString(salt));
+    hexString.append(Base64.getEncoder().encodeToString(hashedPassword));
     System.out.println(hexString.toString());
+    return hexString.toString();
+  }
+
+  public static boolean verifyPassword(String hashPassword, String plainPassword) throws NoSuchAlgorithmException {
+    boolean isVerified = false;
+    String salt = hashPassword.substring(0, 24);
+    byte[] byteSalt = Base64.getDecoder().decode(salt);
+    MessageDigest md = MessageDigest.getInstance("SHA-256");
+    md.reset();
+    md.update(byteSalt);
+    byte[] hashedPassword = md.digest(plainPassword.getBytes(StandardCharsets.UTF_8));
+    StringBuffer hexString = new StringBuffer();
+    hexString.append(Base64.getEncoder().encodeToString(byteSalt));
+    hexString.append(Base64.getEncoder().encodeToString(hashedPassword));
+    System.out.println(hexString.toString());
+    System.out.println(hashPassword);
+    hexString.toString();
+    isVerified = hashPassword.equals(hexString.toString());
+    return isVerified;
   }
 }
